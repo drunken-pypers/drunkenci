c3720feb883610a317766f760c1b973ea9d04afa
RaviShanker B <ravishanker414@gmail.com>
2015-07-12 17:57:02 +0530

    Initial Commit and function to clone a repo

---
+++ project.clj
@@ -0,0 +1,58 @@
+(defproject drunkenci "0.1.0-SNAPSHOT"
+
+  :description "Drunken Ci"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [selmer "0.8.2"]
+                 [com.taoensso/timbre "4.0.2"]
+                 [com.taoensso/tower "3.0.2"]
+                 [markdown-clj "0.9.67"]
+                 [environ "1.0.0"]
+                 [compojure "1.3.4"]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-session-timeout "0.1.0"]
+                 [ring "1.4.0"
+                  :exclusions [ring/ring-jetty-adapter]]
+                 [metosin/ring-middleware-format "0.6.0"]
+                 [metosin/ring-http-response "0.6.3"]
+                 [bouncer "0.3.3"]
+                 [prone "0.8.2"]
+                 [org.clojure/tools.nrepl "0.2.10"]
+                 [migratus "0.8.2"]
+                 [org.clojure/java.jdbc "0.3.7"]
+                 [instaparse "1.4.1"]
+                 [yesql "0.5.0-rc3"]
+                 [com.h2database/h2 "1.4.187"]
+                 [http-kit "2.1.19"]
+
+                 [clj-jgit "0.8.8"]
+                 ]
+
+  :min-lein-version "2.0.0"
+  :uberjar-name "drunkenci.jar"
+  :jvm-opts ["-server"]
+
+;;enable to start the nREPL server when the application launches
+;:env {:repl-port 7001}
+
+  :main drunkenci.core
+  :migratus {:store :database}
+
+  :plugins [[lein-environ "1.0.0"]
+            [lein-ancient "0.6.5"]
+            [migratus-lein "0.1.5"]]
+
+  :profiles
+  {:uberjar {:omit-source true
+             :env {:production true}
+             :aot :all}
+   :dev {:dependencies [[ring-mock "0.1.5"]
+                        [ring/ring-devel "1.4.0"]
+                        [pjstadig/humane-test-output "0.7.0"]]
+
+
+         :repl-options {:init-ns drunkenci.core}
+         :injections [(require 'pjstadig.humane-test-output)
+                      (pjstadig.humane-test-output/activate!)]
+         :env {:dev true}}})
---
+++ src/drunkenci/repo.clj
@@ -0,0 +1,14 @@
+(ns drunkenci.repohandler
+  (:require [clj-jgit.porcelain :as git]
+            [clojure.java.io :as io]
+            [environ.core :refer [env]]))
+
+(defn repo-name-form-url [repo-url]
+  (if-let [match (re-find #"/([a-zA-Z\.\-_]+).git$" repo-url)]
+          (nth match 1)
+          (throw (Exception. (str "Cant get repo name for " repo-url)))))
+
+(defn clone-repo [repo-url]
+  (git/git-clone-full repo-url
+                      (io/file (env :workspace)
+                               (repo-name-form-url repo-url))))
